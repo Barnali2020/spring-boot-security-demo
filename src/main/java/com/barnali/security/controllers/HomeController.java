f45447240ca78d4f00432e8ad7b7433d3d16f8c1
Barnali <mail2barnali1990@gmail.com>
2020-06-09 15:50:18 +0530

    Added Home controller

---
+++ src/main/java/com/barnali/security/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.barnali.security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/")
+	public String home() {
+		return "<h1>Welcome</h1>";
+	}
+
+}
